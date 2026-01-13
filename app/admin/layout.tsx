a8aedcd243fec774992a803e84e23bafb97e7a91
valenottaviano <valentinottaviano@gmail.com>
2026-01-13 12:16:48 -0300

    fix: navbar button

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -79,7 +79,7 @@ export default function AdminLayout({
                     rel="noopener noreferrer"
                     className="px-4 py-2 rounded-xl text-xs font-bold uppercase tracking-widest transition-all text-white/40 hover:text-white hover:bg-white/5"
                   >
-                    Cronograma
+                   Credenciales 
                   </a>
                 </nav>
               </div>
