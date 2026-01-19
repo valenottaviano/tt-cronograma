24454820687ad7ff160b2251c31a731ebbb9f1b4
valenottaviano <valentinottaviano@gmail.com>
2026-01-19 14:12:20 -0300

    fix: reactivated app

--- components/pwa-install-toast.tsx
+++ components/pwa-install-toast.tsx
@@ -10,34 +10,34 @@ export function PWAInstallToast() {
   const { isInstallable, isIOS, isStandalone, handleInstallClick } = usePWAInstall();
   const [hasShownToast, setHasShownToast] = useState(false);
 
-  // useEffect(() => {
-  //   if (hasShownToast) return;
+  useEffect(() => {
+    if (hasShownToast) return;
 
-  //   if (isInstallable) {
-  //     toast("¡Instala nuestra App!", {
-  //       description: "Accede rápidamente a tus beneficios y cronogramas desde nuestra app.",
-  //       duration: 10000,
-  //       action: {
-  //         label: "Instalar",
-  //         onClick: () => handleInstallClick(),
-  //       },
-  //       icon: <Download className="h-5 w-5" />,
-  //     });
-  //     setHasShownToast(true);
-  //   } else if (isIOS && !isStandalone) {
-  //     // Check if they are in Chrome/Firefox on iOS
-  //     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isInstallable) {
+      toast("¡Instala nuestra App!", {
+        description: "Accede rápidamente a tus beneficios y cronogramas desde nuestra app.",
+        duration: 10000,
+        action: {
+          label: "Instalar",
+          onClick: () => handleInstallClick(),
+        },
+        icon: <Download className="h-5 w-5" />,
+      });
+      setHasShownToast(true);
+    } else if (isIOS && !isStandalone) {
+      // Check if they are in Chrome/Firefox on iOS
+      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
       
-  //     toast("Para instalar en iPhone:", {
-  //       description: isSafari 
-  //         ? "Toca el botón 'Compartir' y luego 'Agregar a inicio'." 
-  //         : "Esta app solo se puede instalar usando Safari. Abre este enlace en Safari para instalar.",
-  //       duration: 20000,
-  //       icon: <Download className="h-5 w-5" />,
-  //     });
-  //     setHasShownToast(true);
-  //   }
-  // }, [isInstallable, isIOS, isStandalone, hasShownToast, handleInstallClick]);
+      toast("Para instalar en iPhone:", {
+        description: isSafari 
+          ? "Toca el botón 'Compartir' y luego 'Agregar a inicio'." 
+          : "Esta app solo se puede instalar usando Safari. Abre este enlace en Safari para instalar.",
+        duration: 20000,
+        icon: <Download className="h-5 w-5" />,
+      });
+      setHasShownToast(true);
+    }
+  }, [isInstallable, isIOS, isStandalone, hasShownToast, handleInstallClick]);
 
   return null;
 }
